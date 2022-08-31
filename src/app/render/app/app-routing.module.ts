e4b7ffaf12e1adeabe6a33aa9f421df2d35ce40e
Luis Rodrigues <luis.ms.rodrigues@hotmail.com>
2022-08-31 12:43:37 +0100

    Added initial setup for store

--- src/app/render/app/app-routing.module.ts
+++ src/app/render/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    loadChildren: () => import('./features/starting-point/starting-point.module').then(m => m.StartingPointModule)
   }
 ];
 
