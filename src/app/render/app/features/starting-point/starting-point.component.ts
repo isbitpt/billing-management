140c12805e34f35f8aa25c5d64f57d10fef3291e
Roberto "ResaloliPT" Pires <roberto_pires97@sapo.pt>
2022-09-01 22:37:03 +0000

    Added auth to renderer process

--- src/app/render/app/features/starting-point/starting-point.component.ts
+++ src/app/render/app/features/starting-point/starting-point.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as sharedModels from '../../../../shared';
+import {UserDatabase} from '../../../../shared';
+
+const ipcRenderer = window.require('electron/renderer').ipcRenderer;
 
 @Component({
   selector: 'isbit-home',
@@ -7,11 +11,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./starting-point.component.scss']
 })
 export class StartingPointComponent implements OnInit {
+  public databases: UserDatabase[];
+  public selectedDb = '';
+  public password = '';
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     console.log('StartingPointComponent INIT');
+
+    this.loadDatabases();
   }
 
+  public onDbChange() {
+    this.password = '';
+  }
+
+  public authToDb(): void {
+    ipcRenderer.invoke('auth:canLogin', this.selectedDb, this.password)
+      .then((loggedBd: sharedModels.AuthLoginToBdResult) => {
+        if (!loggedBd.success) {
+          console.error('Failed to login');
+          return;
+        }
+
+        console.log(`Logged in to the Bd '${loggedBd.loggedBd.name}'`);
+      });
+  }
+
+  private loadDatabases(): void {
+    ipcRenderer.invoke('auth:loadDatabases')
+      .then((loadedDatabases: sharedModels.AuthLoadDatabasesResult) => {
+        this.databases = loadedDatabases.databases;
+
+        if (this.databases.length > 0) {
+          this.selectedDb = this.databases[0].id;
+        }
+      });
+  }
 }
--- src/app/render/app/features/starting-point/starting-point.module.ts
+++ src/app/render/app/features/starting-point/starting-point.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { StartingPointRoutingModule } from './starting-point-routing.module';
 import {StartingPointComponent} from './starting-point.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import {StartingPointComponent} from './starting-point.component';
   ],
   imports: [
     CommonModule,
-    StartingPointRoutingModule
+    StartingPointRoutingModule,
+    FormsModule
   ]
 })
 export class StartingPointModule { }
