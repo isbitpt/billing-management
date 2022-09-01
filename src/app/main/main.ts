19d2994e20b099a9b9b93f5b4afac1b5f18f0926
Roberto "ResaloliPT" Pires <roberto_pires97@sapo.pt>
2022-09-01 22:36:31 +0000

    Changed main.ts to better suit async loading

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -4,108 +4,107 @@ import { app, BrowserWindow, screen } from 'electron';
 import * as path from 'path';
 import * as fs from 'fs';
 
+import {EventManager} from './events';
 import {DatabaseManager} from './database';
 
-DatabaseManager.loadAppDatabaseAsync()
-  .then(() => {
-    const appContext = DatabaseManager.appDatabaseContext;
-
-    return appContext.databasesRepository.createNewDatabase(`TestBd${Math.random()}`, '123das');
-  })
-  .then(() => {
-    const appContext = DatabaseManager.appDatabaseContext;
-
-    return appContext.databasesRepository.getAllDatabases();
-  }).then((databases) => {
-
-    console.log(databases);
-});
-
-
-
-let win: BrowserWindow = null;
 const args = process.argv.slice(1);
-  const serve = args.some(val => val === '--serve');
+const serve = args.some(val => val === '--serve');
 
-function createWindow(): BrowserWindow {
+class App {
+  #win: BrowserWindow = null;
 
-  const size = screen.getPrimaryDisplay().workAreaSize;
+  public async startApp(): Promise<void> {
+    const testDb = (process.env.TEST_DB || false ) === '1';
 
-  // Create the browser window.
-  win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
-    webPreferences: {
-      nodeIntegration: true,
-      allowRunningInsecureContent: (serve),
-      contextIsolation: false,  // false if you want to run e2e test with Spectron
-    },
-  });
+    await DatabaseManager.loadAppDatabaseAsync();
 
-  if (serve) {
-    const debug = require('electron-debug');
-    debug();
+    if (testDb) {
+      const appContext = DatabaseManager.appDatabaseContext;
+      await appContext.databasesRepository.createNewDatabase(`TestBd${Math.random()}`, '123das');
 
-    require('electron-reloader')(module);
-    win.loadURL('http://localhost:4200');
-  } else {
-    // Path when running electron executable
-    let pathIndex = './index.html';
-
-    if (fs.existsSync(path.join(__dirname, '../../dist/index.html'))) {
-      // Path when running electron in local folder
-      pathIndex = '../../dist/index.html';
+      const loadedDatabases = await appContext.databasesRepository.getAllDatabases();
+      console.log(loadedDatabases);
     }
 
-    const url = new URL(path.join('file:', __dirname, pathIndex));
-    win.loadURL(url.href);
+    EventManager.registerEvents();
+
+    this.registerElectronEvents();
+
+    await app.whenReady();
+
+    this.createWindow();
   }
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+  private registerElectronEvents() {
+    try {
+      // Quit when all windows are closed.
+      app.on('window-all-closed', () => {
+        // On OS X it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') {
+          app.quit();
+        }
+      });
 
-  return win;
-}
+      app.on('activate', () => {
+        // On OS X it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (this.#win === null) {
+          this.createWindow();
+        }
+      });
 
-function startAppRender(): void {
-  try {
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    // Some APIs can only be used after this event occurs.
-    // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-    app.on('ready', () => setTimeout(createWindow, 400));
+    } catch (e) {
+      // Catch Error
+      // throw e;
+    }
+  }
 
-    // Quit when all windows are closed.
-    app.on('window-all-closed', () => {
-      // On OS X it is common for applications and their menu bar
-      // to stay active until the user quits explicitly with Cmd + Q
-      if (process.platform !== 'darwin') {
-        app.quit();
+  private createWindow(): BrowserWindow {
+
+    const size = screen.getPrimaryDisplay().workAreaSize;
+
+    // Create the browser window.
+    this.#win = new BrowserWindow({
+      x: 0,
+      y: 0,
+      width: size.width,
+      height: size.height,
+      webPreferences: {
+        nodeIntegration: true,
+        allowRunningInsecureContent: (serve),
+        contextIsolation: false,  // false if you want to run e2e test with Spectron
+      },
+    });
+
+    if (serve) {
+      const debug = require('electron-debug');
+      debug();
+
+      require('electron-reloader')(module);
+      this.#win.loadURL('http://localhost:4200');
+    } else {
+      // Path when running electron executable
+      let pathIndex = './index.html';
+
+      if (fs.existsSync(path.join(__dirname, '../../dist/index.html'))) {
+        // Path when running electron in local folder
+        pathIndex = '../../dist/index.html';
       }
+
+      const url = new URL(path.join('file:', __dirname, pathIndex));
+      this.#win.loadURL(url.href);
+    }
+
+    // Emitted when the window is closed.
+    this.#win.on('closed', () => {
+      // Dereference the window object, usually you would store window
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      this.#win = null;
     });
 
-    app.on('activate', () => {
-      // On OS X it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (win === null) {
-        createWindow();
-      }
-    });
-
-    app.on('before-quit', async () => {
-      await DatabaseManager.closeAndSave();
-    });
-
-  } catch (e) {
-    // Catch Error
-    // throw e;
+    return this.#win;
   }
 }
-startAppRender();
+(new App()).startApp();
